ac5a42569255eeb0af3d512a322876b1ff601cb5
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-11-10 11:39:32 +0700

    Initial commit

---
+++ lib/Tables/WordList.pm
@@ -0,0 +1,52 @@
+package Tables::WordList;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+use Role::Tiny::With;
+with 'TablesRole::Source::Iterator';
+
+sub new {
+    require Module::Load::Util;
+
+    my ($class, %args) = @_;
+    defined $args{wordlist} or die "Please specify 'wordlist' argument";
+    $class->_new(
+        gen_iterator => sub {
+            my $wl = Module::Load::Util::instantiate_class_with_optional_args(
+                {ns_prefix=>'WordList'}, $args{wordlist});
+            sub {
+                my $w = $wl->next_word;
+                return undef unless defined $w;
+                {word=>$w};
+            },
+        },
+    );
+}
+
+1;
+# ABSTRACT: Table from a WordList module
+
+=head1 SYNOPSIS
+
+From perl code:
+
+ use Tables::WordList;
+
+ my $table = Tables::WordList->new(wordlist => 'ID::BIP39');
+
+From command-line (using L<tables> CLI):
+
+ % tables show WordList=wordlist,ID::BIP39
+
+
+=head1 SEE ALSO
+
+L<WordList>
+
+L<Tables>
